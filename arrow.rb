6b38961e86d8f670e9df1164cb53de70e464fdc1
beanieboi <beanie@benle.de>
2012-12-15 23:18:35 +0100

    set lane dynamically

--- arrow.rb
+++ arrow.rb
@@ -3,23 +3,24 @@ class Arrow
     @screen = screen
     @speed = 1
     @direction = 1
-    @body = [               [2,0],
-              [0,1], [1,1], [2,1], [3,1],
-                            [2,2]
+    @lane = 0
+    @body = [
+              [2, Proc.new { @lane + 0 }], [0, Proc.new { @lane + 1 }],
+              [1, Proc.new { @lane + 1 }], [2, Proc.new { @lane + 1 }],
+              [3, Proc.new { @lane + 1 }], [2, Proc.new { @lane + 2 }]
             ]
   end
 
   def update(frame_count)
     @lane = [0, 3, 6].sample if frame_count % 15
     @body.each do |el|
-      el[0] = el[0] + (@speed * @direction)
-      el[1] = el[1] + (@speed * @direction)
+      el[0] = (el[0] + (@speed * @direction)) % @screen.width
     end
   end
 
   def draw
     @body.each do |el|
-      @screen[el[0], el[1]] = true
+      @screen[el[0], el[1].call] = true
     end
   end
 end
