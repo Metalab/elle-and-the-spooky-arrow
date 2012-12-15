a27fec324cace1959cbdada8f6c099990657f13b
beanieboi <beanie@benle.de>
2012-12-15 17:28:44 +0100

    reading joystick data from the raw device

--- joystick.rb
+++ joystick.rb
@@ -1,11 +1,29 @@
-require 'usb'
-require 'ostruct'
+require 'fcntl'
 
 class Joystick
   attr_accessor :state
+
   def initialize
-    @dev = USB.devices.select{|d| d.idVendor==0x046d && d.idProduct==0xc21a}.first
+    @device = "/dev/input/js0"
+    @fd = IO::sysopen(@device, Fcntl::O_RDONLY)
     work
+  end
+
+  def actions
+    actions = Array.new
+    actions << Proc.new { |state| print 'left'   if state[7] == 0x00 && state[5] == 0x80 }
+    actions << Proc.new { |state| print 'right'  if state[7] == 0x00 && state[5] == 0x7F }
+    actions << Proc.new { |state| print 'up'     if state[7] == 0x01 && state[5] == 0x80 }
+    actions << Proc.new { |state| print 'down'   if state[7] == 0x01 && state[5] == 0x7F }
+    actions << Proc.new { |state| print 'red'    if state[7] == 0x01 && state[6] == 0x01}
+    actions << Proc.new { |state| print 'yellow' if state[7] == 0x02 && state[6] == 0x01 }
+    actions << Proc.new { |state| print 'green'  if state[7] == 0x03 && state[6] == 0x01 }
+    actions << Proc.new { |state| print 'blue'   if state[7] == 0x00 && state[6] == 0x01 }
+    actions << Proc.new { |state| print 'l1'     if state[7] == 0x06 && state[6] == 0x01 }
+    actions << Proc.new { |state| print 'r1'     if state[7] == 0x07 && state[6] == 0x01 }
+    actions << Proc.new { |state| print 'select' if state[7] == 0x08 && state[6] == 0x01 }
+    actions << Proc.new { |state| print 'start'  if state[7] == 0x09 && state[6] == 0x01 }
+    actions
   end
 
   def action
@@ -15,53 +33,12 @@ class Joystick
   end
 
   def work
-    Thread.start do
-      @dev.open do |h|
-
-        h.usb_detach_kernel_driver_np(0,0) rescue nil
-        data = (0..3).to_.a.pack("C*")
-        endpoint = @dev.endpoints.first.bEndpointAddress
-
-        # clear anything in the buffer
-        loop do
-          begin
-            size = handle.usb_interrupt_read(endpoint, data, -1)
-          rescue
-            break
-          end
-        end
-
-        actions = Array.new
-        actions << Proc.new { |state| @state = 'left'   if state[2] == 0x00 && state[3] == 0x04 }
-        actions << Proc.new { |state| @state = 'right'  if state[0] == 0xFF }
-        actions << Proc.new { |state| @state = 'up'     if state[2] == 0x00 && state[3] == 0x10}
-        actions << Proc.new { |state| @state = 'down'   if state[1] == 0xFF }
-        actions << Proc.new { |state| @state = 'red'    if (state[2] & 0x01) == 0x01 }
-        actions << Proc.new { |state| @state = 'yellow' if (state[2] & 0x02) == 0x02 }
-        actions << Proc.new { |state| @state = 'green'  if (state[2] & 0x04) == 0x04 }
-        actions << Proc.new { |state| @state = 'blue'   if (state[2] & 0x08) == 0x08 }
-        actions << Proc.new { |state| @state = 'l1'     if (state[2] & 0x10) == 0x10 }
-        actions << Proc.new { |state| @state = 'r1'     if (state[2] & 0x20) == 0x20 }
-        actions << Proc.new { |state| @state = 'l2'     if (state[2] & 0x40) == 0x40 }
-        actions << Proc.new { |state| @state = 'r2'     if (state[2] & 0x80) == 0x80 }
-        actions << Proc.new { |state| @state = 'select' if (state[3] & 0x01) == 0x01 }
-        actions << Proc.new { |state| @state = 'start'  if (state[3] & 0x02) == 0x02 }
-
-        loop do
-          begin
-            h.usb_bulk_read(endpoint, data, 10)
-            state_array = data.unpack("C*")
-            actions.each { |a| a.call(state_array) }
-          rescue Errno::ETIMEDOUT => e
-          rescue Interrupt
-            exit 130
-          end
-          sleep(0.1)
-        end
-      end
-    end
+    f = IO.open(@fd)
+    loop {
+      data = f.read(8).unpack("C*")
+      actions.each { |a| a.call(data) }
+    }
   end
 end
 
-
-j = Blinkofant::Pong::Joystick.new
+j = Joystick.new
