61e44da35a386082ec53e64c8465143b7a34eeb0
beanieboi <beanie@benle.de>
2012-12-15 22:43:06 +0100

    remove benchmark

--- game.rb
+++ game.rb
@@ -15,15 +15,9 @@ class Game
     p1 = Pixel.new(screen)
 
     loop do
-      _start = Time.now.usec
-
       p1.action(j1.action)
       p1.draw
       device.flush
-
-      _end = Time.now.usec
-      puts "#{(_end - _start)/1000.0} ms"
-
       sleep(1/@@fps.to_f)
     end
   end
