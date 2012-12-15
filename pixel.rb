f864a135c010544a30742b29c371713d065f903c
beanieboi <beanie@benle.de>
2012-12-15 21:06:05 +0100

    catch out of bounds

--- pixel.rb
+++ pixel.rb
@@ -7,10 +7,10 @@ class Pixel
 
   def action(action)
     case action
-    when :right then @x += 1
-    when :left then @x -= 1
-    when :down then @y += 1
-    when :up then @y -= 1
+    when :right then  @x = (@x + 1) % screen.width
+    when :left then   @x = (@x - 1) % screen.width
+    when :down then   @y = (@y + 1) % screen.height
+    when :up then     @y = (@y - 1) % screen.height
     end
   end
 
