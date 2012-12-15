7ec7b0954537694213c4c74ed80eedef03ae442c
beanieboi <beanie@benle.de>
2012-12-15 18:16:19 +0100

    init outside the game loop! noob! :)

--- game.rb
+++ game.rb
@@ -7,17 +7,14 @@ class Game
   @@fps = 25
 
   def self.run
+    screen = Screen.new
+    device = Device.new(screen)
+    j1 = Joystick.new
+    p1 = Pixel.new(screen)
+
     loop {
-      pixel = []
-
-      screen = Screen.new
-      device = Device.new(screen)
-      j1 = Joystick.new
-      p1 = Pixel.new(screen)
-
       p1.action(j1.action)
       p1.draw
-
       device.flush
       sleep(1/@@fps.to_f)
     }
