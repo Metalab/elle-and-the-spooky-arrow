06576ef8b008f1474c8bb51bec267e7f68714019
beanieboi <beanie@benle.de>
2012-12-15 21:32:22 +0100

    don't run outside the cube

--- pixel.rb
+++ pixel.rb
@@ -9,8 +9,8 @@ class Pixel
     case action
     when :right then  @x = (@x + 1) % @screen.width
     when :left then   @x = (@x - 1) % @screen.width
-    when :down then   @y = (@y + 1) % @screen.height
-    when :up then     @y = (@y - 1) % @screen.height
+    when :down then   @y = [@y + 1, @screen.height - 1].min
+    when :up then     @y = [@y - 1, 0].max
     end
   end
 
