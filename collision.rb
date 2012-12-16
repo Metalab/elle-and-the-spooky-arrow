10262d5943247a7aadee0d940a2e5a9644c420ea
beanieboi <beanie@benle.de>
2012-12-16 16:07:07 +0100

    update collision detection

--- collision.rb
+++ collision.rb
@@ -1,8 +1,9 @@
 class Collision
   def collide?(arrow, guy)
-    front_pixel = arrow.body[4]
-    back_pixel = guy.body[0]
+    arrow_front_col = arrow.body[4][0]
+    guy_back_col = guy.body[0][0]
 
-    arrow.lane == guy.lane && front_pixel[0] == back_pixel[0]
+    (arrow.lane == guy.lane) && (arrow_front_col == guy_back_col ||
+                                 arrow_front_col == guy_back_col + 1)
   end
 end
