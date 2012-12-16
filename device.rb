a7ac8859a50b0ddc8aeb155795d013f9b4064928
beanieboi <beanie@benle.de>
2012-12-16 17:05:35 +0100

    optimize Device#flush

--- device.rb
+++ device.rb
@@ -7,20 +7,30 @@ class Device
 
   def initialize(screen)
     @screen = screen
+    init_spi
+    init_gpio
+  end
+
+  def init_spi
     @device = '/dev/spidev0.0'
+    @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
+    @f = IO.open(@fd)
+    @f.sync = true
+    @f.ioctl(SPI_IOC_WR_MAX_SPEED_HZ, [100_000].pack("L"))
+  end
+
+  def init_gpio
     @io = WiringPi::GPIO.new
     @io.mode(DARKEN_PIN,OUTPUT)
   end
 
   def flush
+    packed_stream = @screen.bit_stream.pack("C*")
+
     @io.write(DARKEN_PIN,LOW)
-    @fd = IO::sysopen(@device, Fcntl::O_WRONLY)
-    f = IO.open(@fd)
-    f.sync = true
-    f.ioctl(SPI_IOC_WR_MAX_SPEED_HZ, [100_000].pack("L"))
-    f.write(@screen.bit_stream.pack("C*"))
-    f.close
-    @screen.reset
+    @f.write(packed_stream)
     @io.write(DARKEN_PIN,HIGH)
+
+    @screen.reset
   end
 end
